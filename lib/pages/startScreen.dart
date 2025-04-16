9460074203e98827b1d0703f1bb1d766215d68ce
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-16 15:29:49 +0200

    done with the first page sorta was just chaning the items

--- lib/pages/startScreen.dart
+++ lib/pages/startScreen.dart
@@ -6,13 +6,24 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(
-            "images/startscreen.png",
-            width: 250,
-          )
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "images/startscreen.png",
+              width: 250,
+            ),
+            Text(
+              "uFulu",
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "A Comprehensive GBV Prevention and\nResponse Aplication",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
       ),
     );
   }
