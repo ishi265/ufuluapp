da764ecd84c53992279ad265bd2c4cc1d88de687
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-16 15:21:02 +0200

    added fonts in the themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ufuluapp/pages/startScreen.dart';
+import 'package:ufuluapp/theme/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: mainColors.copyWith(
+        textTheme: GoogleFonts.kellySlabTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: StartScreen(),
     );
