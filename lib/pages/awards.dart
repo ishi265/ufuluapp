bbde867f7fb6e1d6d84de874a543e7799a2ffced
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-16 20:02:50 +0200

    awards part

--- lib/pages/awards.dart
+++ lib/pages/awards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class Awards extends StatelessWidget {
   const Awards({super.key});
@@ -10,10 +11,39 @@ class Awards extends StatelessWidget {
           child: Padding(
         padding: EdgeInsets.all(10.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Awards",
               style: TextStyle(fontSize: 38, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "Your Knowledge in SGBV",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Iconsax.award5,
+                        color: Theme.of(context).colorScheme.primary,
+                        size: 30,
+                      ),
+                      Text(
+                        "Basics of SGBV",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  
+                ],
+              ),
             )
           ],
         ),
