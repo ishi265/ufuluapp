3ff817f2cf26f415d3dcdee6a7d1da0be8f7fa16
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-16 18:48:43 +0200

    Service widget seppareted right now

---
+++ lib/widgets/ServiceWidget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ServiceWidget extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Image image;
+
+  const ServiceWidget(
+      {super.key, required this.text, required this.icon, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              text,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Icon(
+                  icon,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 20,
+                ),
+                image
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
