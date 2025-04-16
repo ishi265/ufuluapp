1a6b2277abb7712c4a01c65389b829212d3c7ffa
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-16 15:11:48 +0200

    changes in the code

--- lib/pages/startScreen.dart
+++ lib/pages/startScreen.dart
@@ -1,1 +1,16 @@
-import
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
