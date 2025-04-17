3df6c7085d5055e7594eeded5b4c791339560fb6
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-17 15:19:02 +0200

    created an assignment page in here

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ufuluapp/pages/video.dart';
+import 'package:ufuluapp/pages/assignment.dart';
 import 'package:ufuluapp/theme/theme.dart';
 
 void main() {
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: VideoPage(),
+      home: AssignmetPage(),
     );
   }
 }
---
+++ lib/pages/assignment.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class AssignmetPage extends StatelessWidget {
+  const AssignmetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Iconsax.arrow_left),
+        title: Text("Assignment One"),
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Question 1/5",
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              "1 Point",
+              style: TextStyle(fontSize: 16),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                "Why is it important to observe GBV around your peers.",
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
