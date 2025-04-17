0b5c789c9cea41547514983d263c2d86d55b526a
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2025-04-17 10:56:43 +0200

    grades widget created in the system

--- lib/pages/courseRoadMap.dart
+++ lib/pages/courseRoadMap.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:ufuluapp/widgets/GradesWidget.dart';
 
 class CourseRoadMap extends StatelessWidget {
   const CourseRoadMap({super.key});
@@ -10,6 +11,50 @@ class CourseRoadMap extends StatelessWidget {
       appBar: AppBar(
         leading: Icon(Iconsax.arrow_left),
         title: Text("Health Relationships"),
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Builder(
+                builder: (context) {
+                  double screenWidth = MediaQuery.of(context).size.width;
+                  double tabWidth = screenWidth / 2;
+                  double horizontalInset = tabWidth * 0.53;
+
+                  return TabBar(
+                    indicator: UnderlineTabIndicator(
+                      borderSide: BorderSide(
+                        width: 3,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                      insets: EdgeInsets.symmetric(horizontal: horizontalInset),
+                    ),
+                    labelColor: Theme.of(context).colorScheme.onPrimary,
+                    unselectedLabelColor:
+                        Theme.of(context).colorScheme.onPrimary,
+                    dividerColor: Colors.grey[200],
+                    tabs: const [
+                      Tab(text: 'Home'),
+                      Tab(text: 'Grades'),
+                    ],
+                  );
+                },
+              ),
+              const Divider(height: 0.001),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Center(child: Text("Course Road Map")),
+                    GradesWidget(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/GradesWidget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GradesWidget extends StatelessWidget {
+  const GradesWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text("We start her creating the item")],
+    );
+  }
+}
